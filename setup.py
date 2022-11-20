7a59b65de5c6531686f6636bd2a3b82a4c90656b
Ed Summers <ehs@pobox.com>
2022-11-20 06:32:38 -0500

    fix scripts in setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 def long_description():
     with open("README.md") as f:
@@ -21,6 +21,6 @@ setup(
     zip_safe=True,
     entry_points="""
         [console_scripts]
-        "twitter_archive_unshorten" = twitter_archive_unshorten:main
+        twitter_archive_unshorten = twitter_archive_unshorten:main
     """,
 )
